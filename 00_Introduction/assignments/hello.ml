19eddfe586cf76cb59e5a4419dee849aef07e79f
Logan Knecht <lknecht@splunk.com>
2017-11-07 18:04:24 -0800

    General first lesson work.

---
+++ 00_Introduction/assignments/hello.ml
@@ -0,0 +1,7 @@
+open Printf
+ 
+let message = "Hello world";;
+(printf "%s\n" message)
+
+let food = "Burritos";;
+(printf "I like spicy %s!\n" food)
---
+++ 00_Introduction/videos/VIDEO.md
@@ -0,0 +1,2 @@
+# Video Links
+- http://beet.cs.swarthmore.edu/jpolitz/cs75-videos/01.1-jan-19-720p.mov
