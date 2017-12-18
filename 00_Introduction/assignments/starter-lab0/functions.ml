e6ed108154facd50abcca1265a7c03fa942572b1
Logan Knecht <lknecht@splunk.com>
2017-12-18 15:21:43 -0800

    Solve fibonacci and perform substitution calculation.

--- 00_Introduction/assignments/starter-lab0/functions.ml
+++ 00_Introduction/assignments/starter-lab0/functions.ml
@@ -1,2 +1,34 @@
-(* Fill in the functions you need to write here *)
+open Printf
 
+(* let fibonacci_index = 0 *)
+(* let fibonacci_index = 1 *)
+(* let fibonacci_index = 2 *)
+(* let fibonacci_index = 3 *)
+(* let fibonacci_index = 4 *)
+(* let fibonacci_index = 5 *)
+(* let fibonacci_index = 6 *)
+let fibonacci_index = 7
+
+(* Fill in the functions you need to write here *)
+let rec calculate_fibonacci (index : int): int =
+    if index == 0 then 0
+    else if index == 1 then 1
+    else (calculate_fibonacci (index - 2)) + (calculate_fibonacci (index - 1));;
+
+(printf "Fibonacci of %d is %d\n" fibonacci_index (calculate_fibonacci fibonacci_index))
+
+
+
+
+(* -------------------------------------------------------------------------- *)
+(* Fibonacci substitution style *)
+(* -------------------------------------------------------------------------- *)
+(*
+(calculate_fibonacci 3)
+(calculate_fibonacci 3 - 2) + (calculate_fibonacci 3 - 1)
+
+(calculate_fibonacci 2)
+(calculate_fibonacci 2 - 2) + (calculate_fibonacci 2 - 1)
+(calculate_fibonacci 1)
+(calculate_fibonacci 1 - 2) + (calculate_fibonacci 1 - 1)
+ *)
